67ebf083a0d5b6095132e3235d96bb6ee694d4a0
Rholex Dizon <dizonrholex02@gmail.com>
2026-08-31 17:47:01 +0800

    chore: remove machine-specific database configuration

--- EmployeeManagementSystem/SalaryData.cs
+++ EmployeeManagementSystem/SalaryData.cs
@@ -19,7 +19,7 @@ namespace EmployeeManagementSystem
         public int Salary {  get; set; } //3
 
 
-        SqlConnection connect = new SqlConnection(@"Data Source=RHOLEX-PC\SQLEXPRESS;Initial Catalog=EmployeeManagementSystem;Integrated Security=True;Connect Timeout=30");
+        SqlConnection connect = new SqlConnection(Environment.GetEnvironmentVariable("EMPLOYEE_MANAGEMENT_DB_CONNECTION") ?? throw new InvalidOperationException("EMPLOYEE_MANAGEMENT_DB_CONNECTION must be set before starting the application"));
 
         public List<SalaryData> salaryListData()
     {
